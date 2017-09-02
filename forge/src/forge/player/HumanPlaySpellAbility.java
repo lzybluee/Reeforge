d80db122f4ea6f07a2708730264f1073070a2bca
lzybluee <lzybluee@gmail.com>
2017-09-02 14:12:59 +0800

    No need to announce X when casting <Spell Burst>

--- forge/src/forge/player/HumanPlaySpellAbility.java
+++ forge/src/forge/player/HumanPlaySpellAbility.java
@@ -322,12 +322,27 @@ public class HumanPlaySpellAbility {
             }
             if (xInCost) {
                 final String sVar = ability.getSVar("X"); //only prompt for new X value if card doesn't determine it another way
-                if ("Count$xPaid".equals(sVar) || sVar.isEmpty()) {
-                    final Integer value = controller.announceRequirements(ability, "X", allowZero && manaCost.canXbe0());
-                    if (value == null) {
-                        return false;
+
+                boolean noNeedToChooseX = false;
+                String tgtType = ability.getParam("TargetType");
+                if(sVar.isEmpty() && "Spell".equals(tgtType)) {
+                    Card host = ability.getHostCard();
+                    if(host != null) {
+                        String v = ability.getHostCard().getSVar("X");
+                        if("Targeted$CardManaCost".equals(v)) {
+                            noNeedToChooseX = true;
+                        }
                     }
-                    card.setXManaCostPaid(value);
+                }
+
+                if(!noNeedToChooseX) {
+                    if ("Count$xPaid".equals(sVar) || sVar.isEmpty()) {
+                        final Integer value = controller.announceRequirements(ability, "X", allowZero && manaCost.canXbe0());
+                        if (value == null) {
+                            return false;
+                        }
+                        card.setXManaCostPaid(value);
+                    }
                 }
             } else if (manaCost.getMana().isZero() && ability.isSpell()) {
                 card.setXManaCostPaid(0);
