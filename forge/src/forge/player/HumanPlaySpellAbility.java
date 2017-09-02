a1e4246b28d59b79933a5f83259bf5ccdeb04989
lzybluee <lzybluee@gmail.com>
2017-09-02 13:09:42 +0800

    Refund all mana paid when cancel trigger ability's payment

--- forge/src/forge/player/HumanPlaySpellAbility.java
+++ forge/src/forge/player/HumanPlaySpellAbility.java
@@ -148,12 +148,18 @@ public class HumanPlaySpellAbility {
 
         // This line makes use of short-circuit evaluation of boolean values, that is each subsequent argument
         // is only executed or evaluated if the first argument does not suffice to determine the value of the expression
-        final boolean prerequisitesMet = announceValuesLikeX()
+        boolean prerequisitesMet = announceValuesLikeX()
                 && announceType()
-                && (!mayChooseTargets || setupTargets()) // if you can choose targets, then do choose them.
-                && (isFree || payment.payCost(new HumanCostDecision(controller, human, ability, ability.getHostCard())));
+                && (!mayChooseTargets || setupTargets()); // if you can choose targets, then do choose them.
+
+        boolean payCost = isFree || payment.payCost(new HumanCostDecision(controller, human, ability, ability.getHostCard()));
+
+        prerequisitesMet = (prerequisitesMet && payCost);
 
         if (!prerequisitesMet) {
+            if (ability.isTrigger() && !payCost) {
+                payment.refundPayment();
+            }
             if (!ability.isTrigger()) {
                 rollbackAbility(fromZone, fromState, zonePosition);
                 if (ability.getHostCard().isMadness()) {
