3a23f0153a94315f7e9631b78ce3e80cb349c07d
lzybluee <lzybluee@gmail.com>
2017-09-02 23:02:07 +0800

    Fix remove elements in loop

--- forge/src/forge/game/ability/effects/AnimateAllEffect.java
+++ forge/src/forge/game/ability/effects/AnimateAllEffect.java
@@ -159,16 +159,22 @@ public class AnimateAllEffect extends AnimateEffectBase {
                 }
             }
 
+            boolean clearAbilities = sa.hasParam("OverwriteAbilities");
+            boolean removeAll = sa.hasParam("RemoveAllAbilities");
+
             // remove abilities
             final List<SpellAbility> removedAbilities = new ArrayList<SpellAbility>();
-            if (sa.hasParam("OverwriteAbilities") || sa.hasParam("RemoveAllAbilities")) {
+            if (clearAbilities || removeAll) {
                 for (final SpellAbility ab : c.getSpellAbilities()) {
-                    if (ab.isAbility()) {
-                        c.removeSpellAbility(ab);
+                    if (removeAll || (ab.isAbility() && clearAbilities)) {
                         removedAbilities.add(ab);
                     }
                 }
+                for(final SpellAbility ab : removedAbilities) {
+                    c.removeSpellAbility(ab);
+                }
             }
+
             // give replacement effects
             final List<ReplacementEffect> addedReplacements = new ArrayList<ReplacementEffect>();
             if (replacements.size() > 0) {
@@ -178,6 +184,7 @@ public class AnimateAllEffect extends AnimateEffectBase {
                     addedReplacements.add(c.addReplacementEffect(parsedReplacement));
                 }
             }
+
             // Grant triggers
             final List<Trigger> addedTriggers = new ArrayList<Trigger>();
             if (triggers.size() > 0) {
