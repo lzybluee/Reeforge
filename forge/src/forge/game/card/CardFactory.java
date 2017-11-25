0fb88d4a9e939eb4fb56b963400da1888283544f
lzybluee <lzybluee@gmail.com>
2017-11-25 16:32:53 +0800

    Fix <Spelltwine> copy charm spells

--- forge/src/forge/game/card/CardFactory.java
+++ forge/src/forge/game/card/CardFactory.java
@@ -243,6 +243,11 @@ public class CardFactory {
             }
             copySA.setPaidHash(sa.getPaidHash());
         }
+
+        if(!original.getZone().is(ZoneType.Stack)) {
+            copySA.setSVar("CanSelectCharmEffect", "true");
+        }
+
         return copySA;
     }
 
