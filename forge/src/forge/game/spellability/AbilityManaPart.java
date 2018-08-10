2c613923e63b0ae828b8b6edcd2f7278060b554a
lzybluee <lzybluee@gmail.com>
2018-08-11 00:03:46 +0800

    Fix <Eldrazi Temple> can provide mana for trigger abilities again See 3d7b96df50cf833ec9122069c827de0d81d6ab6a

--- forge/src/forge/game/spellability/AbilityManaPart.java
+++ forge/src/forge/game/spellability/AbilityManaPart.java
@@ -338,15 +338,16 @@ public class AbilityManaPart implements java.io.Serializable {
                 }
             }
 
+            if (sa.isAbility() && restriction.startsWith("Activated") && sa.isTrigger()) {
+            	continue;
+            }
+
             if (sa.isValid(restriction, this.getSourceCard().getController(), this.getSourceCard(), null)) {
                 return true;
             }
 
             if (sa.isAbility()) {
                 if (restriction.startsWith("Activated")) {
-                    if(sa.isTrigger()) {
-                        continue;
-                    }
                     restriction = TextUtil.fastReplace(restriction, "Activated", "Card");
                 }
                 else {
