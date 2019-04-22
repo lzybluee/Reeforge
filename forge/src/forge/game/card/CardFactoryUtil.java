1d534d83fd5d3590616bd297b7d4c39343c39f9e
lzybluee <lzybluee@gmail.com>
2019-04-22 10:59:20 +0800

    Adjust some accidentally commits

--- forge/src/forge/game/card/CardFactoryUtil.java
+++ forge/src/forge/game/card/CardFactoryUtil.java
@@ -2479,7 +2479,7 @@ public class CardFactoryUtil {
                 + "| Origin$ Any | Destination$ Battlefield "
                 + "| TriggerZones$ Battlefield | OptionalDecider$ You "
                 + "| IsPresent$ Card.Self+counters_GE1_P1P1"
-                + "| Secondary$ True | TriggerDescription$"
+                + "| Secondary$ True | TriggerDescription$ "
                 + "Whenever another creature enters the battlefield, you "
                 + "may move a +1/+1 counter from this creature onto it.";
             final Trigger trigger = TriggerHandler.parseTrigger(trigStr, card, intrinsic);
