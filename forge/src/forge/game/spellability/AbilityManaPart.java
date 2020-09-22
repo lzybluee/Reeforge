31cbbb1be03264b4937f1ed67905f0f444183aec
lzybluee <lzybluee@gmail.com>
2020-09-22 08:02:09 +0800

    Add back "MorphOrManifest" creature check, now it works for non-creature morph cards

--- forge/src/forge/game/spellability/AbilityManaPart.java
+++ forge/src/forge/game/spellability/AbilityManaPart.java
@@ -342,7 +342,7 @@ public class AbilityManaPart implements java.io.Serializable {
                 continue;
             }
             if (restriction.equals("MorphOrManifest")) {
-                if ((sa.isSpell() && sa.isCastFaceDown())
+                if ((sa.isSpell() && sa.getHostCard().isCreature() && sa.isCastFaceDown())
                         || sa.isManifestUp() || sa.isMorphUp()) {
                     return true;
                 } else {
